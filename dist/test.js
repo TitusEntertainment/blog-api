a7785976ca08c69764f874e11e9aeae4988a8ec3
Titus <properllerbucher@gmail.com>
2019-10-07 23:20:23 +0200

    removed dist

--- dist/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AuthorModel_1 = require("./models/AuthorModel");
-const newAuthor = new AuthorModel_1.Author({
-    name: 'Eddie Englund',
-    born: 20011222,
-    biography: 'this is jus ta test',
-});
-exports.default = newAuthor;
